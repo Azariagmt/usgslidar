e44196bbe096746215dd34994c2f3283a15bc6f8
Azariagmt <azariagebremichael10@gmail.com>
2021-08-19 06:28:32 +0300

    fixes setup file for pypi pub

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import setup, find_packages
- 
+
 classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Education',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
 ]
- 
+
 setup(
-  name='usgs-lidar',
-  version='0.0.1',
-  description='A basic package to fetch usgs agriculture data',
-  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='',  
-  author='Azaria Gebremichael',
-  author_email='azariagebremichael10@gmail.com',
-  license='MIT', 
-  classifiers=classifiers,
-  keywords='usgs', 
-  packages=find_packages(),
-  install_requires=[''] 
+    name='usgs-lidar',
+    version='0.0.1',
+    description='A basic package to fetch usgs agriculture data',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read() + '\n\n' +
+    open('CHANGELOG.txt').read(),
+    url='',
+    author='Azaria Gebremichael',
+    author_email='azariagebremichael10@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='usgs',
+    packages=find_packages(),
+    install_requires=['']
 )
