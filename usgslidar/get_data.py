93ced34c6b8d52340d13948252e3e78826b8a1cd
Azariagmt <azariagebremichael10@gmail.com>
2021-08-21 00:44:20 +0300

    initial get data fix

--- usgslidar/get_data.py
+++ usgslidar/get_data.py
@@ -1,7 +1,13 @@
 import pdal
 import json
-import geopandas
+import geopandas as gpd
 from pyproj import Transformer
+from logs import log
+from shapely.geometry import Polygon
+
+
+logger = log(path="../logs/", file="get_data.logs")
+logger.info("Starts Get data script")
 
 PUBLIC_DATA_PATH = "https://s3-us-west-2.amazonaws.com/usgs-lidar-public/"
 
@@ -11,45 +17,114 @@ MINX, MINY, MAXX, MAXY = [-93.756155, 41.918015, -93.747334, 41.921429]
 BOUNDS = f"({[MINX,MAXX]},{[MINY,MAXY]})"
 BOUNDS = "([-11669524, -11666600.8],[4776607.3, 4778714.4])"
 
-
-transformer = Transformer.from_crs("EPSG:3857", "EPSG:4326", always_xy=True)
-xmin, ymax = transformer.transform(MINX, MAXY)
-xmax, ymin = transformer.transform(MAXX, MINY)
-pdal_aoi_bounds = f"([{xmin}, {xmax}], [{ymin}, {ymax}])"
-
-
 PUBLIC_ACCESS_PATH = PUBLIC_DATA_PATH+REGION+"ept.json"
 
-OUTPUT_FILENAME_LAZ = "laz/SoPlatteRiver.laz"
-OUTPUT_FILENAME_TIF = "tif/SoPlatteRiver.tif"
+# TODO ask user to input filename
+OUTPUT_FILENAME_LAZ = "../data/laz/SoPlatteRiver.las"
+OUTPUT_FILENAME_TIF = "../data/tif/SoPlatteRiver.tif"
 
 PIPELINE_PATH = "get_data.json"
 
 
-def get_raster_terrain(bounds: str, region: str, PUBLIC_ACCESS_PATH: str = PUBLIC_ACCESS_PATH,
+def get_polygon_boundaries(polygon: Polygon, input_epsg : int = 3857, output_epsg :int = 4326):
+    """Gets the required polygon boundaries
+
+    Arguments:
+        Polygon -- A shapely polygon
+                   https://shapely.readthedocs.io/en/stable/manual.html
+        Input epsg -- The input CRS reference system
+                      default -- 3857
+        Output epsg -- The output CRS reference system
+                      default -- 26915
+    
+    Returns: A tuple of lists containing rectangular boundary
+            ([minx, maxx],[miny,maxy]) &
+            the initial user polygon input
+    
+    """
+    polygon_df = gpd.GeoDataFrame([polygon], columns=['geometry'])
+
+    polygon_df.set_crs(epsg=output_epsg, inplace=True)
+    polygon_df['geometry'] = polygon_df['geometry'].to_crs(
+        epsg=input_epsg)
+    minx, miny, maxx, maxy = polygon_df['geometry'][0].bounds
+
+    polygon_input = 'POLYGON(('
+
+    xcord, ycord = polygon_df['geometry'][0].exterior.coords.xy
+    for x, y in zip(list(xcord), list(ycord)):
+        polygon_input += f'{x} {y}, '
+    polygon_input = polygon_input[:-2]
+    polygon_input += '))'
+    
+    print(polygon_input)
+
+    return f"({[minx, maxx]},{[miny,maxy]})", polygon_input
+
+
+def get_raster_terrain(polygon: list, region: str, PUBLIC_ACCESS_PATH: str = PUBLIC_ACCESS_PATH,
                        OUTPUT_FILENAME_LAZ: str = OUTPUT_FILENAME_LAZ, OUTPUT_FILENAME_TIF: str = OUTPUT_FILENAME_TIF,
                        PIPELINE_PATH: str = PIPELINE_PATH
                        ) -> None:
-    with open(PIPELINE_PATH) as json_file:
-        the_json = json.load(json_file)
+    """[Initializes the PDAL pipeline and gets bound for entire region]
 
-    the_json['pipeline'][0]['bounds'] = bounds
+    Arguments: 
+        Polygon (list of x,y coordinate tuples) -- sets boundary
+        region (string) -- region
+    
+    Optional Arguments:
+        PIPELINE_PATH (string) -- path to new PDAL pipeline
 
-    the_json['pipeline'][0]['filename'] = PUBLIC_ACCESS_PATH
+    Returns:
+        nothing yet
+    """
+    try:
+        input_polygon = Polygon(polygon)
+        MINX, MINY, MAXX, MAXY = [-93.756155, 41.918015, -93.747334, 41.921429]
+        input_polygon = Polygon(((MINX, MINY), (MINX, MAXY), (MAXX, MAXY), (MAXX, MINY), (MINX, MINY)))
+        logger.info("Polygon input success!")
+    except RuntimeError as e:
+        logger.exception("Polygon input error")
+        
+    try:
+        bounds, polygon_input = get_polygon_boundaries(input_polygon)
+        logger.info("Rectangular input extracted successfully")
+    except RuntimeError as e:
+        logger.exception("Polygon extraction error")
 
-    the_json['pipeline'][3]['filename'] = OUTPUT_FILENAME_LAZ
-    the_json['pipeline'][4]['filename'] = OUTPUT_FILENAME_TIF
 
-    pipeline = pdal.Pipeline(json.dumps(the_json))
+    # TODO convert CRS to 3857
 
+    try:
+        with open(PIPELINE_PATH) as json_file:
+            the_json = json.load(json_file)
+
+        the_json['pipeline'][0]['bounds'] = bounds
+        the_json['pipeline'][0]['filename'] = PUBLIC_ACCESS_PATH
+
+        the_json['pipeline'][1]['polygon'] = polygon_input
+
+        the_json['pipeline'][4]['filename'] = OUTPUT_FILENAME_LAZ
+        # the_json['pipeline'][6]['filename'] = OUTPUT_FILENAME_TIF
+
+        pipeline = pdal.Pipeline(json.dumps(the_json))
+
+        logger.info("pipeline initiated")
+    except RuntimeError as e:
+        logger.exception("failed to initiate pipeline")
     try:
 
         pipe_exec = pipeline.execute()
+        # TODO all the comments
+        # list df numpy arrays...x, y and z bounds
+        # laz file contains a lot of info
+        # interested in elevation point
+        # z is the elevation
         metadata = pipeline.metadata
-
+        logger.info("Pipeline executed and metadata generated successfully")
     except RuntimeError as e:
-        print(e)
+        logger.exception("Failed to generate metadata and execute pipeline")
 
 
 if __name__ == "__main__":
-    get_raster_terrain(bounds=BOUNDS, region=REGION)
+    get_raster_terrain(polygon=[(-93.756155, 41.918015),( -93.747334, 41.921429), (-93.756155, 41.918015), (-93.756155, 41.918015) ], region="IA_FullState")
